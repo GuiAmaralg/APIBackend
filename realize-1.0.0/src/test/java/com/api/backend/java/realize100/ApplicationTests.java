038cc01e7c2d2ce99a935102735c77a8032c8706
Guilherme Amaral <guilhermeamaral_15@hotmail.com>
2020-08-27 21:50:54 -0300

    My first commit

---
+++ realize-1.0.0/src/test/java/com/api/backend/java/realize100/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.backend.java.realize100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
